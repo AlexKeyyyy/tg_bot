dcb2fbf6dc67f367a5d0b5fa00393b66cf818b48
Alex Koba_Desktop <koba.ayu@edu.spbstu.ru>
2024-03-10 04:39:14 +0300

    fix db access

--- db.js
+++ db.js
@@ -2,8 +2,8 @@ const {Sequelize} = require('sequelize');
 
 module.exports = new Sequelize(
     'tg_bot',
-    'root',
-    'root',
+    'root1',
+    'root1',
     {
         host: 'master.e0d355c7-4ce7-493a-a689-40a9b9e0bf13.c.dbaas.selcloud.ru',
         port: '5432',
